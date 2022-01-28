738aa24e161558757027c29f5e9ff8a138aa99d0
Akshay R <aksr4@publicisgroupe.net>
2022-01-28 13:34:30 +0530

    webpack configured and installed dependencies for react and webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index.bundle.js',
+    },
+    devServer: {
+        port: 3010,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(eot|woff|ttf|gif|png|jpe?g|svg)$/i,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+};
