9e55ea515200846dbe0ff1c1835d69ace26b9964
Akshay R <aksr4@publicisgroupe.net>
2022-02-06 21:46:04 +0530

    added a header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
 import Logo from "../../../static/images/logo.png";
+import { setFilter } from '../../redux/actions/productAction';
 import './Header.scss'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const { item } = useSelector((state) => state);
+  const dispatch = useDispatch()
   return (
     <>
-      <header className='container'>
+    <div className='header'>
+      <header className='header-container'>
         <nav className='nav-container'>
           <Link to="/" rel="Home">
             <img
@@ -24,14 +30,14 @@ const Header = () => {
               </Link>
             </li>
             <li>
-              <Link to="/products" role="link">
+              <Link to="/products" role="link" onClick={()=>dispatch(setFilter(null))}>
                 Products
               </Link>
             </li>
           </ul>
         </nav>
         <div className="cart-login-container">
-          <nav>
+          
             <ul className="list-h">
               <li className="link">
                 <Link to="/login" role="link">
@@ -44,15 +50,22 @@ const Header = () => {
                 </Link>
               </li>
             </ul>
-          </nav>
-          <button
-            className="btn-cart"
-            onClick={() => {
-              setOpenCart(true);
-            }}
-          >Cart</button>
+         
+          <div className='cart-box'>
+            <Link 
+            to="/cart"
+            className='cart-btn'>
+              <FontAwesomeIcon
+              icon={faShoppingCart}
+              alt="Cart Icon"
+              className="cart-icon"
+            />
+              {item ? item : 0} items
+            </Link>
+          </div>
         </div>
       </header>
+      </div>
     </>
   );
 };
