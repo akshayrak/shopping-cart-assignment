47349a5055bf77535d5a4d851923a387019da64c
Akshay R <aksr4@publicisgroupe.net>
2022-02-06 21:46:57 +0530

    added carousel

---
+++ src/components/carousel/Carousel.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Flickity from "react-flickity-component";
+import "./Carousel.scss"
+
+const Carousel = ({ imageData }) => {
+
+  const flickityOptions = { autoPlay: true }
+  return (
+    <div className='carousel-container'>
+      <Flickity options={flickityOptions} className='flickity-tag'>
+      {imageData.map(data => {
+        return <>
+          <div key={data.id}>
+            <img
+              src={`https://raw.githubusercontent.com/akshayrak/shopping-cart-assignment/master${data.bannerImageUrl}`}
+              alt={data.bannerImageAlt}
+              className='carousel-img'
+            />
+          </div>
+
+        </>
+      })}
+    </Flickity>
+    </div>
+  );
+};
+
+export default Carousel;
