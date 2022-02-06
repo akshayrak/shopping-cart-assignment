29a7e08ee7940d9be949adede402e704dc98393b
Akshay R <aksr4@publicisgroupe.net>
2022-02-06 21:54:09 +0530

    api setup

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 let url="http://localhost:9000"
 export default {
     bannersURL: `${url}/banners`,
-    productsURL: `${url}products`,
-    categoriesURL: `${url}categories`,
+    productsURL: `${url}/products`,
+    categoriesURL: `${url}/categories`,
   };
   
